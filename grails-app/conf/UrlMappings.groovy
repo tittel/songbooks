0006fec6b3450d6c56a307fc27b592bf74597ffe
Stephan Tittel <sot@gmx.net>
2012-10-26 15:06:21 +0200

    test

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,12 +1,11 @@
 class UrlMappings {
-
 	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
+		"/api/$controller" {
+			action = [GET: "list", POST: "create"]
 		}
-
+		"/api/$controller/$id" {
+			action = [GET: "show", PUT: "update", DELETE: "delete", POST: "save"]
+		}
 		"/"(view:"/index")
 		"500"(view:'/error')
 	}
