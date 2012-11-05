3fe8ac7c4048317aa28c287c5e27d566b8c8d8ec
Stephan Tittel <sot@gmx.net>
2012-11-05 21:32:08 +0100

    test

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,4 @@
-// locations to search for config files that get merged into the main config;
+// locations to search for config files that get merged into the main config; 
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
 
